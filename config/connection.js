a842c65d8e0a28de080a934d31379b08c205da28
Alwan Alyafi Mulyawan <alwanalyafi7@gmail.com>
2020-11-30 20:12:24 +0700

    refresh connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,6 @@
 const mysql = require('mysql');
 let connection = "";
     
-    // connection = mysql.createConnection({
-    //     host:'db4free.net:3306',
-    //     user:'admin_warehouse',
-    //     password:'admin_warehouse',
-    //     database:'db_warehouse'
-    // });
-
     connection = mysql.createPool({
         connectionLimit:100,
         host:'db4free.net',
@@ -16,10 +9,5 @@ let connection = "";
         password:'admin_warehouse',
         database:'db_warehouse'
     })
-// connection.connect((err)=>{
-//     if(err)throw err;
-    
-//     console.log('Mysql Connected');
-// });
 
 module.exports = connection;
