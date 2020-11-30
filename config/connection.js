bc59f2cdac689220e5c9d67900337db304b31040
Alwan Alyafi Mulyawan <alwanalyafi7@gmail.com>
2020-11-30 20:08:20 +0700

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,25 @@
 const mysql = require('mysql');
 let connection = "";
-
-    connection = mysql.createConnection({
-        host:'localhost',
-        user:'root',
-        password:'',
-        database:'db_warehouse'
-    });
-
-connection.connect((err)=>{
-    if(err)throw err;
     
-    console.log('Mysql Connected');
-});
+    // connection = mysql.createConnection({
+    //     host:'db4free.net:3306',
+    //     user:'admin_warehouse',
+    //     password:'admin_warehouse',
+    //     database:'db_warehouse'
+    // });
+
+    connection = mysql.createPool({
+        connectionLimit:100,
+        host:'db4free.net',
+        port:3306,
+        user:'admin_warehouse',
+        password:'admin_warehouse',
+        database:'db_warehouse'
+    })
+// connection.connect((err)=>{
+//     if(err)throw err;
+    
+//     console.log('Mysql Connected');
+// });
 
 module.exports = connection;
